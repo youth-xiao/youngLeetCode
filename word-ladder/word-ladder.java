f43774f90aa8e9c5ef55c04d45bad5db129faa4c
Xiaoyang Zou <54784964+youth-xiao@users.noreply.github.com>
2021-11-06 20:59:59 -0700

    Time: 133 ms (44.62%), Space: 108.1 MB (22.52%) - LeetHub

---
+++ word-ladder/word-ladder.java
@@ -0,0 +1,53 @@
+class Solution {
+    public int ladderLength(String beginWord, String endWord, List<String> wordList) {
+        Set<String> wordSet = new HashSet<>(wordList);
+        if (wordSet.size() == 0 || !wordSet.contains(endWord)) {
+            return 0;
+        }
+        wordSet.remove(beginWord);
+        
+        Queue<String> queue = new LinkedList<>();
+        queue.add(beginWord);
+        Set<String> visited = new HashSet<>();
+        visited.add(beginWord);
+        
+        int step = 1;
+        while (!queue.isEmpty()) {
+            int currSize = queue.size();
+            for (int i = 0; i < currSize; i++) {
+                String currWord = queue.poll();
+                if (changeOneLetter(currWord, endWord, queue, wordSet, visited)) {
+                    return step + 1;
+                }
+            }
+            step++;
+        }
+        return 0;
+    }
+    
+    private boolean changeOneLetter(String currWord, String endWord, 
+                                    Queue<String> queue, Set<String> wordSet, Set<String> visited) {
+        char[] wordArray = currWord.toCharArray();
+        for (int i = 0; i < currWord.length(); i++) {
+            char oldChar = wordArray[i];
+            for (char k = 'a'; k <= 'z'; k++) {
+                if (k == oldChar) {
+                    continue;
+                }
+                wordArray[i] = k;
+                String newWord = String.valueOf(wordArray);
+                if (wordSet.contains(newWord)) {
+                    if (newWord.equals(endWord)) {
+                        return true;
+                    }
+                    if (!visited.contains(newWord)) {
+                        queue.add(newWord);
+                        visited.add(newWord);
+                    }
+                }
+            }
+            wordArray[i] = oldChar;
+        }
+        return false;
+    }
+}
