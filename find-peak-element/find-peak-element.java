ff0fbeb4318b76a5c1ecc5dfbdeca58a1ffef08a
Xiaoyang Zou <54784964+youth-xiao@users.noreply.github.com>
2021-11-29 11:22:44 -0800

    Time: 0 ms (100.00%), Space: 38.4 MB (96.38%) - LeetHub

--- find-peak-element/find-peak-element.java
+++ find-peak-element/find-peak-element.java
@@ -1,38 +1,36 @@
-// class Solution {
-//     public int findPeakElement(int[] nums) {
-//         if (nums.length < 3) {
-//             return 0;
-//         }
-//         int left = 0;
-//         int right = nums.length - 1;
-//         while (left < right) {
-//             int mid = left + (right - left) / 2;
-//             if (nums[mid] > nums[mid - 1] && nums[mid] > nums[mid + 1]) {
-//                 return mid;
-//             } else if (nums[mid] > nums[mid - 1] && nums[mid] <= nums[mid + 1]) {
-//                 left = mid + 1;
-//             } else if (nums[mid] <= nums[mid - 1] && nums[mid] > nums[mid + 1]) {
-//                 right = mid + 1;
-//             }
-//         }
-//         return -1;
-//     }
-// }
-
-
 class Solution {
     public int findPeakElement(int[] nums) {
-        return search(nums, 0, nums.length - 1);
-    }
-    
-    public int search(int[] nums, int left, int right) {
-        if (left == right) {
-            return left;
+        int left = 0;
+        int right = nums.length - 1;
+        while (left <= right) {
+            if (left == right) {
+                return right;
+            }
+            int mid = (right + left) / 2;
+            if (nums[mid] > nums[mid + 1]) {
+                right = mid;
+            } else {
+                left = mid + 1;
+            }
         }
-        int mid = (left + right) / 2;
-        if (nums[mid] > nums[mid + 1]) {
-            return search(nums, left, mid);
-        }
-        return search(nums, mid + 1, right);
+        return -1;
     }
 }
+
+
+// class Solution {
+//     public int findPeakElement(int[] nums) {
+//         return search(nums, 0, nums.length - 1);
+//     }
+    
+//     public int search(int[] nums, int left, int right) {
+//         if (left == right) {
+//             return left;
+//         }
+//         int mid = (left + right) / 2;
+//         if (nums[mid] > nums[mid + 1]) {
+//             return search(nums, left, mid);
+//         }
+//         return search(nums, mid + 1, right);
+//     }
+// }
