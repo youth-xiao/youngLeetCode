e53324138a9f2f4f2263cf39845d0e7fd7a3b1d8
Xiaoyang Zou <54784964+youth-xiao@users.noreply.github.com>
2021-12-02 14:09:07 -0800

    Time: 0 ms (100.00%), Space: 36.4 MB (56.45%) - LeetHub

--- house-robber/house-robber.java
+++ house-robber/house-robber.java
@@ -1,3 +1,45 @@
+class Solution {
+    private int[] memo;
+    
+    public int rob(int[] nums) {
+        this.memo = new int[nums.length];
+        Arrays.fill(this.memo, -1);
+        return this.robFrom(0, nums);
+    }
+    
+    private int robFrom(int i, int[] nums) {
+        if (i >= nums.length) {
+            return 0;
+        }
+        if (this.memo[i] > -1) {
+            return this.memo[i];
+        }
+        int ans = Math.max(this.robFrom(i + 1, nums), this.robFrom(i + 2, nums) + nums[i]);
+        this.memo[i] = ans;
+        return ans;
+    }
+}
+
+
+// class Solution {
+//     public int rob(int[] nums) {
+//         int len = nums.length;
+//         if (nums == null || len == 0) return 0;
+//         int[] dp = new int[len];
+//         dp[0] = nums[0];
+//         if (len == 1) return dp[0];
+//         dp[1] = Math.max(nums[0], nums[1]);
+//         if (len == 2) return dp[1];
+        
+//         for (int i = 2; i < len; i++) {
+//             dp[i] = Math.max(dp[i - 1], nums[i] + dp[i - 2]);
+//         }
+        
+//         return dp[len - 1];
+//     }
+// }
+
+
 // class Solution {
 //     public int rob(int[] nums) {
 //         int n = nums.length;
@@ -9,22 +51,3 @@
 //         return dp[n];
 //     }
 // }
-
-
-class Solution {
-    public int rob(int[] nums) {
-        int len = nums.length;
-        if (nums == null || len == 0) return 0;
-        int[] dp = new int[len];
-        dp[0] = nums[0];
-        if (len == 1) return dp[0];
-        dp[1] = Math.max(nums[0], nums[1]);
-        if (len == 2) return dp[1];
-        
-        for (int i = 2; i < len; i++) {
-            dp[i] = Math.max(dp[i - 1], nums[i] + dp[i - 2]);
-        }
-        
-        return dp[len - 1];
-    }
-}
