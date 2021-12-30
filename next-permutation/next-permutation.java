1dd1f88dfbf5b54b194ae554c7e64bc3f2368ef0
Xiaoyang Zou <54784964+youth-xiao@users.noreply.github.com>
2021-12-29 17:51:07 -0800

    Time: 1 ms (36.97%), Space: 40.3 MB (13.47%) - LeetHub

---
+++ next-permutation/next-permutation.java
@@ -0,0 +1,39 @@
+class Solution {
+    public void nextPermutation(int[] nums) {
+        int n = nums.length;
+        int i = n - 2;
+        while (i >= 0 && nums[i] >= nums[i + 1]) {
+            i--;
+        }
+        
+        if (i < 0) {
+            reverse(nums, 0);
+            return;
+        }
+        
+        int j = n - 1;
+        while (j >= 0 && nums[j] <= nums[i]) {
+            j--;
+        }
+        
+        swap(nums, i, j);
+        reverse(nums, i + 1);
+        
+    }
+    
+    private void swap(int[] nums, int i, int j) {
+        int temp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = temp;
+    }
+    
+    private void reverse(int[] nums, int i) {
+        int left = i;
+        int right = nums.length - 1;
+        while (left < right) {
+            swap(nums, left, right);
+            left++;
+            right--;
+        }
+    }
+}
