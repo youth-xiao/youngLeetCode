b8e163b0e5e3e07c2cea27e1a05775e3c0718767
Xiaoyang Zou <54784964+youth-xiao@users.noreply.github.com>
2022-01-02 01:15:14 -0800

    Time: 12 ms (30.20%), Space: 41.4 MB (17.03%) - LeetHub

--- longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.java
+++ longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.java
@@ -17,24 +17,64 @@
 // }
 
 
+
+// // SLIDING WINDOW
+// class Solution {
+//     public int lengthOfLongestSubstring(String s) {
+//         if (s.length() == 0) return 0;
+//         int[] chars = new int[128]; // the array only stores the frequency of characters within the sliding window, not within the whole array
+//         int maxLength = 1;
+//         int left = 0;
+//         int right = 0;
+//         while (right < s.length()) {
+//             char r = s.charAt(right);
+//             chars[r]++;
+//             while (chars[r] > 1) { // this means the current sliding window contains duplicates
+//                 char l = s.charAt(left);
+//                 chars[l]--; // we reduce the frequency of that duplicated character before we increment the lower bound of the sliding window
+//                 left++; // shifting the lower bound to the right to eliminate duplicates
+//             }
+//             maxLength = Math.max(maxLength, right - left + 1);
+//             right++; // no matter what, we increment right for every loop b/c we want to check all the characters
+//         }
+//         return maxLength;
+//     }
+// }
+
+// // Hashmap
+// class Solution {
+//      public int lengthOfLongestSubstring(String s) {
+//          Map<Character, Integer> map = new HashMap<>();
+//          int start = 0;
+//          int maxLength = 0;
+//          for (int i = 0; i < s.length(); i++) {
+//              char c = s.charAt(i);
+//              if (map.containsKey(c)) { // duplicates found
+//                  if (map.get(c) >= start) { // when the duplicate is within the sliding window
+//                     start = map.get(c) + 1; // we shifting the lower bound of the sliding window to the right
+//                  }
+//              }
+//              maxLength = Math.max(maxLength, i - start + 1);
+//              map.put(c, i); // PUT it anyways, no matter if the map contains the key or not
+//          }
+//          return maxLength;
+//      }
+// }
+
+// One pass
 class Solution {
     public int lengthOfLongestSubstring(String s) {
-        if (s.length() == 0) return 0;
-        int[] chars = new int[128];
-        int maxLength = 1;
+        Map<Character, Integer> map = new HashMap<>();
         int left = 0;
-        int right = 0;
-        while (right < s.length()) {
-            char r = s.charAt(right);
-            chars[r]++;
-            while (chars[r] > 1) {
-                char l = s.charAt(left);
-                chars[l]--;
-                left++;
+        int res = 0;
+        for (int i = 0; i < s.length(); i++) { // aka. right pointer
+            char c = s.charAt(i);
+            if (map.containsKey(c)) {
+                left = Math.max(left, map.get(c)); // get the right-most index
             }
-            maxLength = Math.max(maxLength, right - left + 1);
-            right++;
+            map.put(c, i + 1);
+            res = Math.max(res, i - left + 1);
         }
-        return maxLength;
+        return res;
     }
 }
