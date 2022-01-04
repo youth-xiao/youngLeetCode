8e9a7e7c09ee654ad8ba2095e903e732d29363d2
Xiaoyang Zou <54784964+youth-xiao@users.noreply.github.com>
2022-01-03 17:56:08 -0800

    Time: 3 ms (96.82%), Space: 38.6 MB (98.75%) - LeetHub

--- 76-minimum-window-substring/76-minimum-window-substring.java
+++ 76-minimum-window-substring/76-minimum-window-substring.java
@@ -1,42 +1,73 @@
 class Solution {
-    public String minWindow (String s, String t) {
-        Map<Character, Integer> map = new HashMap<>();
+    public String minWindow(String s, String t) {
+        int[] map = new int[128];
         for (char c : t.toCharArray()) {
-            map.put(c, map.getOrDefault(c, 0) + 1);
+            map[c]++;
         }
-        
-        int left = 0;
-        int minStart = 0;
-        int res = Integer.MAX_VALUE;
-        int validCount = 0;
-        
-        for (int i = 0; i < s.length(); i++) {
-            char curr = s.charAt(i);
-            if (map.containsKey(curr)) {
-                if (map.get(curr) > 0) {
-                    validCount++;
-                }
-                map.put(curr, map.get(curr) - 1);
+        int start = 0, end = 0, minStart = 0, minLen = Integer.MAX_VALUE, count = t.length();
+        while (end < s.length()) {
+            char c1 = s.charAt(end);
+            if (map[c1] > 0) {
+                count--;
             }
-            
-            while (validCount == t.length()) {
-                if (i - left + 1 < res) {
-                    res = i - left + 1;
-                    minStart = left;
+            map[c1]--;
+            end++;
+            while (count == 0) {
+                if (minLen > end - start) {
+                    minLen = end - start;
+                    minStart = start;
                 }
-                char l = s.charAt(left);
-                if (map.containsKey(l)) {
-                    map.put(l, map.getOrDefault(l, 0) + 1);
-                    if (map.get(l) > 0) {
-                        validCount--;
-                    }
+                char c2 = s.charAt(start);
+                map[c2]++;
+                if (map[c2] > 0) {
+                    count++;
                 }
-                left++;
+                start++;
             }
         }
-        return res == Integer.MAX_VALUE ? "" : s.substring(minStart, minStart + res);
+        return minLen == Integer.MAX_VALUE ? "" : s.substring(minStart, minStart + minLen);
     }
 }
+
+// class Solution {
+//     public String minWindow (String s, String t) {
+//         Map<Character, Integer> map = new HashMap<>();
+//         for (char c : t.toCharArray()) {
+//             map.put(c, map.getOrDefault(c, 0) + 1);
+//         }
+        
+//         int left = 0;
+//         int minStart = 0;
+//         int minLength = Integer.MAX_VALUE;
+//         int validCount = 0;
+        
+//         for (int i = 0; i < s.length(); i++) {
+//             char curr = s.charAt(i);
+//             if (map.containsKey(curr)) {
+//                 if (map.get(curr) > 0) {
+//                     validCount++;
+//                 }
+//                 map.put(curr, map.get(curr) - 1);
+//             }
+            
+//             while (validCount == t.length()) {
+//                 if (i - left + 1 < minLength) {
+//                     minLength = i - left + 1;
+//                     minStart = left;
+//                 }
+//                 char l = s.charAt(left);
+//                 if (map.containsKey(l)) {
+//                     map.put(l, map.getOrDefault(l, 0) + 1);
+//                     if (map.get(l) > 0) {
+//                         validCount--;
+//                     }
+//                 }
+//                 left++;
+//             }
+//         }
+//         return minLength == Integer.MAX_VALUE ? "" : s.substring(minStart, minStart + minLength);
+//     }
+// }
 
 // class Solution {
 //     public String minWindow(String s, String t) {
