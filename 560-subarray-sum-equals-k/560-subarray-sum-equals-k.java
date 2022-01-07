75437735ba83090b728d8a569b9abda8a8845436
Xiaoyang Zou <54784964+youth-xiao@users.noreply.github.com>
2022-01-06 17:03:55 -0800

    Time: 2524 ms (5.02%), Space: 53 MB (23.08%) - LeetHub

--- 560-subarray-sum-equals-k/560-subarray-sum-equals-k.java
+++ 560-subarray-sum-equals-k/560-subarray-sum-equals-k.java
@@ -1,17 +1,39 @@
+// class Solution {
+//     public int subarraySum(int[] nums, int k) {
+//         int count = 0;
+//         int sum = 0;
+//         // Map<sum, count>
+//         Map<Integer, Integer> map = new HashMap<>();
+//         map.put(0, 1); // initially, sum = 0, and there is just one zero
+        
+//         for (int i = 0; i < nums.length; i++) {
+//             sum += nums[i];
+//             if (map.containsKey(sum - k)) {
+//                 count += map.get(sum - k);
+//             }
+//             map.put(sum, map.getOrDefault(sum, 0) + 1);
+//         }
+//         return count;
+//     }
+// }
+
+
 class Solution {
     public int subarraySum(int[] nums, int k) {
         int count = 0;
-        int sum = 0;
-        // Map<sum, count>
-        Map<Integer, Integer> map = new HashMap<>();
-        map.put(0, 1); // initially, sum = 0, and there is just one zero
+        int[] prefixSum = new int[nums.length + 1];
+        prefixSum[0] = 0; // don't forget this line
         
-        for (int i = 0; i < nums.length; i++) {
-            sum += nums[i];
-            if (map.containsKey(sum - k)) {
-                count += map.get(sum - k);
+        for (int i = 1; i <= nums.length; i++) { // start with i = 1, not i = 0
+            prefixSum[i] = nums[i - 1] + prefixSum[i - 1];
+        }
+        
+        for (int left = 0; left < nums.length; left++) {
+            for (int right = left + 1; right <= nums.length; right++) {
+                if (prefixSum[right] - prefixSum[left] == k) {
+                    count++;
+                }
             }
-            map.put(sum, map.getOrDefault(sum, 0) + 1);
         }
         return count;
     }
