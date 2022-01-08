9a17c96602870b73de891b1d3c3d71356a4bbe22
Xiaoyang Zou <54784964+youth-xiao@users.noreply.github.com>
2022-01-07 17:11:23 -0800

    Time: 1 ms (99.46%), Space: 39.8 MB (57.88%) - LeetHub

--- 45-jump-game-ii/45-jump-game-ii.java
+++ 45-jump-game-ii/45-jump-game-ii.java
@@ -1,18 +1,32 @@
+// class Solution {
+//     public int jump(int[] nums) {
+//         int start = 0, end = 1;
+//         int ans = 0;
+        
+//         while (end < nums.length) {
+//             int maxPos = 0;
+//             for (int i = start; i < end; i++) {
+//                 maxPos = Math.max(maxPos, i + nums[i]);
+//             }
+//             start = end;
+//             end = maxPos + 1;
+//             ans++;
+//         }
+        
+//         return ans;
+//     }
+// }
+
 class Solution {
     public int jump(int[] nums) {
-        int start = 0, end = 1;
-        int ans = 0;
-        
-        while (end < nums.length) {
-            int maxPos = 0;
-            for (int i = start; i < end; i++) {
-                maxPos = Math.max(maxPos, i + nums[i]);
+        int ans = 0, end = 0, maxPos = 0;
+        for (int i = 0; i < nums.length - 1; i++) {
+            maxPos = Math.max(maxPos, i + nums[i]);
+            if (i == end) {
+                end = maxPos;
+                ans++;
             }
-            start = end;
-            end = maxPos + 1;
-            ans++;
         }
-        
         return ans;
     }
 }
