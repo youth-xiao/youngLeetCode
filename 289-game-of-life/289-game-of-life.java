3a779dd04115d240199b9d1ee7f7e4415b1d6b37
Xiaoyang Zou <54784964+youth-xiao@users.noreply.github.com>
2022-01-08 17:39:58 -0800

    Time: 0 ms (100.00%), Space: 37.4 MB (65.74%) - LeetHub

---
+++ 289-game-of-life/289-game-of-life.java
@@ -0,0 +1,40 @@
+class Solution {
+    public void gameOfLife(int[][] board) {
+        int row = board.length;
+        int col = board[0].length;
+        int[][] copyBoard = new int[row][col];
+        
+        for (int i = 0; i < row; i++) {
+            for (int j = 0; j < col; j++) {
+                copyBoard[i][j] = board[i][j];
+            }
+        }
+        
+        int[] neighbor = {0, 1, -1};
+        
+        for (int i = 0; i < row; i++) {
+            for (int j = 0; j < col; j++) {
+                int liveCells = 0;
+                for (int m = 0; m < 3; m++) {
+                    for (int n = 0; n < 3; n++) {
+                        if (!(neighbor[m] == 0 && neighbor[n] == 0)) {
+                            int r = i + neighbor[m];
+                            int c = j + neighbor[n];
+                            if ((r < row && r >= 0) && (c < col && c >= 0) && copyBoard[r][c] == 1) {
+                                liveCells++;
+                            }
+                        }
+                    }
+                }
+                
+                if ((copyBoard[i][j] == 1) && (liveCells < 2 || liveCells > 3)) {
+                    board[i][j] = 0;
+                }
+                
+                if (copyBoard[i][j] == 0 && liveCells == 3) {
+                    board[i][j] = 1;
+                }
+            }
+        }
+    }
+}
