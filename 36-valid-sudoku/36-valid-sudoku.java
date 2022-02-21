b16f3e92fad9c697721d4ff5742887c1f2c62f48
Xiaoyang Zou <54784964+youth-xiao@users.noreply.github.com>
2022-02-21 00:52:28 -0800

    Time: 2 ms (92.68%), Space: 42.2 MB (75.33%) - LeetHub

--- 36-valid-sudoku/36-valid-sudoku.java
+++ 36-valid-sudoku/36-valid-sudoku.java
@@ -1,3 +1,45 @@
+class Solution {
+    public boolean isValidSudoku(char[][] board) {
+        int N = 9;
+        HashSet<Character>[] rows = new HashSet[N];
+        HashSet<Character>[] cols = new HashSet[N];
+        HashSet<Character>[] boxes = new HashSet[N];
+        
+        for (int i = 0; i < N; i++) {
+            rows[i] = new HashSet<>();
+            cols[i] = new HashSet<>();
+            boxes[i] = new HashSet<>();
+        }
+        
+        for (int r = 0; r < N; r++) {
+            for (int c = 0; c < N; c++) {
+                char ch = board[r][c];
+                if (ch == '.') {
+                    continue;
+                }
+                
+                if (rows[r].contains(ch)) {
+                    return false;
+                }
+                rows[r].add(ch);
+                
+                if (cols[c].contains(ch)) {
+                    return false;
+                }
+                cols[c].add(ch);
+                
+                int boxIdx = (r / 3) * 3 + c / 3;
+                if (boxes[boxIdx].contains(ch)) {
+                    return false;
+                }
+                boxes[boxIdx].add(ch);
+            }
+        }
+        return true;
+    }
+}
+
+
 // class Solution {
 //     public boolean isValidSudoku(char[][] board) {
 //         Map<Integer, Set<Integer>> row = new HashMap<>(),
@@ -29,50 +71,3 @@
 //         return true;
 //     }
 // }
-
-class Solution {
-    public boolean isValidSudoku(char[][] board) {
-        int N = 9;
-
-        // Use hash set to record the status
-        HashSet<Character>[] rows = new HashSet[N];
-        HashSet<Character>[] cols = new HashSet[N];
-        HashSet<Character>[] boxes = new HashSet[N];
-        for (int r = 0; r < N; r++) {
-            rows[r] = new HashSet<Character>();
-            cols[r] = new HashSet<Character>();
-            boxes[r] = new HashSet<Character>();
-        }
-
-        for (int r = 0; r < N; r++) {
-            for (int c = 0; c < N; c++) {
-                char val = board[r][c];
-
-                // Check if the position is filled with number
-                if (val == '.') {
-                    continue;
-                }
-
-                // Check the row
-                if (rows[r].contains(val)) {
-                    return false;
-                }
-                rows[r].add(val);
-
-                // Check the column
-                if (cols[c].contains(val)) {
-                    return false;
-                }
-                cols[c].add(val);
-
-                // Check the box
-                int idx = (r / 3) * 3 + c / 3;
-                if (boxes[idx].contains(val)) {
-                    return false;
-                }
-                boxes[idx].add(val);
-            }
-        }
-        return true;
-    }
-}
