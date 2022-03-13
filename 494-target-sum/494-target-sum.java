eff0b91b892be8a9415b1d7429f1401495472c7f
Xiaoyang Zou <54784964+youth-xiao@users.noreply.github.com>
2022-03-12 21:52:34 -0800

    Time: 6 ms (92.57%), Space: 43.5 MB (42.37%) - LeetHub

---
+++ 494-target-sum/494-target-sum.java
@@ -0,0 +1,26 @@
+class Solution {
+    public int findTargetSumWays(int[] nums, int target) {
+        int sum = 0;
+        for (int i : nums) {
+            sum += i;
+        }
+        int diff = sum - target;
+        if (diff < 0 || diff % 2 != 0) {
+            return 0;
+        }
+        int neg = diff / 2; // neg = (sum - target) / 2
+        int n = nums.length;
+        int[][] dp = new int[n + 1][neg + 1];
+        dp[0][0] = 1;
+        for (int i = 1; i <= n; i++) {
+            int num = nums[i - 1]; // because dp[][] starts at index 1, not 0
+            for (int j = 0; j <= neg; j++) {
+                dp[i][j] = dp[i - 1][j];
+                if (j >= num) {
+                    dp[i][j] += dp[i - 1][j - num];
+                }
+            }
+        }
+        return dp[n][neg];
+    }
+}
