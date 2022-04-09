6ebabbae1ddbc753ddaed0eeb36c01da79fc4657
Xiaoyang Zou <54784964+youth-xiao@users.noreply.github.com>
2022-04-08 17:39:57 -0700

    Time: 2 ms (36.28%), Space: 46.1 MB (10.77%) - LeetHub

--- 768-max-chunks-to-make-sorted-ii/768-max-chunks-to-make-sorted-ii.java
+++ 768-max-chunks-to-make-sorted-ii/768-max-chunks-to-make-sorted-ii.java
@@ -1,29 +1,48 @@
+// class Solution {
+//     public int maxChunksToSorted(int[] arr) {
+//         int n = arr.length;
+//         int pivotMax[] = new int[n];
+        
+//         pivotMax[0] = arr[0];
+        
+//         for (int i = 1; i < n; i++) {
+//             pivotMax[i] = Math.max(arr[i], pivotMax[i - 1]);
+//         }
+        
+//         int pivotMin[] = new int[n];
+//         pivotMin[n - 1] = arr[n - 1];
+        
+//         for (int i = n - 2; i >= 0; i--) {
+//             pivotMin[i] = Math.min(arr[i], pivotMin[i + 1]);
+//         }
+        
+//         int count = 0;
+        
+//         for (int i = 0; i < n - 1; i++) {
+//             if (pivotMax[i] <= pivotMin[i + 1]) {
+//                 count++;
+//             }
+//         }
+        
+//         return count + 1;
+//     }
+// }
+
+
 class Solution {
     public int maxChunksToSorted(int[] arr) {
-        int n = arr.length;
-        int pivotMax[] = new int[n];
-        
-        pivotMax[0] = arr[0];
-        
-        for (int i = 1; i < n; i++) {
-            pivotMax[i] = Math.max(arr[i], pivotMax[i - 1]);
-        }
-        
-        int pivotMin[] = new int[n];
-        pivotMin[n - 1] = arr[n - 1];
-        
-        for (int i = n - 2; i >= 0; i--) {
-            pivotMin[i] = Math.min(arr[i], pivotMin[i + 1]);
-        }
-        
-        int count = 0;
-        
-        for (int i = 0; i < n - 1; i++) {
-            if (pivotMax[i] <= pivotMin[i + 1]) {
-                count++;
+        LinkedList<Integer> stack = new LinkedList<>();
+        for (int i : arr) {
+            if (!stack.isEmpty() && i < stack.getLast()) {
+                int head = stack.removeLast();
+                while (!stack.isEmpty() && i < stack.getLast()) {
+                    stack.removeLast();
+                }
+                stack.addLast(head);
+            } else {
+                stack.addLast(i);
             }
         }
-        
-        return count + 1;
+        return stack.size();
     }
 }
