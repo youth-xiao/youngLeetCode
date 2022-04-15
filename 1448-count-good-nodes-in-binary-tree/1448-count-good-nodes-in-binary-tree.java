d14de8b418a86a734cebd4a0a87edcf24a578d56
Xiaoyang Zou <54784964+youth-xiao@users.noreply.github.com>
2022-04-14 21:55:53 -0700

    Time: 23 ms (5.38%), Space: 53.1 MB (80.92%) - LeetHub

--- 1448-count-good-nodes-in-binary-tree/1448-count-good-nodes-in-binary-tree.java
+++ 1448-count-good-nodes-in-binary-tree/1448-count-good-nodes-in-binary-tree.java
@@ -13,25 +13,62 @@
  *     }
  * }
  */
-class Solution {
+
+
+class Pair {
+    TreeNode node;
+    int maxSofar;
     
-    private int good = 0;
-    
-    public int goodNodes(TreeNode root) {
-        dfs(root, Integer.MIN_VALUE);
-        return good;
-    }
-    
-    private void dfs(TreeNode node, int maxSoFar) {
-        if (node.val >= maxSoFar) {
-            good++;
-        }
-        if (node.left != null) {
-            dfs(node.left, Math.max(maxSoFar, node.val));
-        }
-        if (node.right != null) {
-            dfs(node.right, Math.max(maxSoFar, node.val));
-        }
-        
+    Pair(TreeNode node, int maxSofar) {
+        this.node = node;
+        this.maxSofar = maxSofar;
     }
 }
+
+class Solution {
+    public int goodNodes(TreeNode root) {
+        Stack<Pair> stack = new Stack<>();
+        stack.push(new Pair(root, Integer.MIN_VALUE));
+        int good = 0;
+        while (!stack.isEmpty()) {
+            Pair p = stack.pop();
+            TreeNode n = p.node;
+            int currMax = p.maxSofar;
+            
+            if (n.val >= currMax) {
+                good++;
+            }
+            if (n.left != null) {
+                stack.push(new Pair(n.left, Math.max(currMax, n.val)));
+            }
+            if (n.right != null) {
+                stack.push(new Pair(n.right, Math.max(currMax, n.val)));
+            }
+        }
+        return good;
+    }
+}
+
+
+// class Solution {
+    
+//     private int good = 0;
+    
+//     public int goodNodes(TreeNode root) {
+//         dfs(root, Integer.MIN_VALUE);
+//         return good;
+//     }
+    
+//     private void dfs(TreeNode node, int maxSoFar) {
+//         if (node.val >= maxSoFar) {
+//             good++;
+//         }
+//         if (node.left != null) {
+//             dfs(node.left, Math.max(maxSoFar, node.val));
+//         }
+//         if (node.right != null) {
+//             dfs(node.right, Math.max(maxSoFar, node.val));
+//         }
+        
+//     }
+// }
