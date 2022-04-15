6963160d672047aada923d2e2aa160b076ba2498
Xiaoyang Zou <54784964+youth-xiao@users.noreply.github.com>
2022-04-14 23:02:28 -0700

    Time: 0 ms (100.00%), Space: 40.2 MB (81.69%) - LeetHub

---
+++ 339-nested-list-weight-sum/339-nested-list-weight-sum.java
@@ -0,0 +1,61 @@
+/**
+ * // This is the interface that allows for creating nested lists.
+ * // You should not implement it, or speculate about its implementation
+ * public interface NestedInteger {
+ *     // Constructor initializes an empty nested list.
+ *     public NestedInteger();
+ *
+ *     // Constructor initializes a single integer.
+ *     public NestedInteger(int value);
+ *
+ *     // @return true if this NestedInteger holds a single integer, rather than a nested list.
+ *     public boolean isInteger();
+ *
+ *     // @return the single integer that this NestedInteger holds, if it holds a single integer
+ *     // Return null if this NestedInteger holds a nested list
+ *     public Integer getInteger();
+ *
+ *     // Set this NestedInteger to hold a single integer.
+ *     public void setInteger(int value);
+ *
+ *     // Set this NestedInteger to hold a nested list and adds a nested integer to it.
+ *     public void add(NestedInteger ni);
+ *
+ *     // @return the nested list that this NestedInteger holds, if it holds a nested list
+ *     // Return empty list if this NestedInteger holds a single integer
+ *     public List<NestedInteger> getList();
+ * }
+ */
+
+
+// class Pair {
+//     NestedInteger nest;
+//     int weight;
+    
+//     Pair(NestedInteger nest, int weight) {
+//         this.nest = nest;
+//         this.weight = weight;
+//     }
+// }
+class Solution {
+    public int depthSum(List<NestedInteger> nestedList) {
+        int total = 0;
+        int weight = 1;
+        Queue<NestedInteger> queue = new LinkedList<>();
+        queue.addAll(nestedList);
+        while (!queue.isEmpty()) {
+            int size = queue.size();
+            for (int i = 0; i < size; i++) {
+                 NestedInteger ni = queue.poll();
+                if (ni.isInteger()) {
+                    total += (ni.getInteger() * weight);
+                } else {
+                    queue.addAll(ni.getList());
+                }
+            }
+            weight++;
+        }
+        return total;
+        
+    }
+}
