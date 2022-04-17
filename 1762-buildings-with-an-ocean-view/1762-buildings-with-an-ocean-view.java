659cd06675e1d936eaf8f1417a89c0aa242a066f
Xiaoyang Zou <54784964+youth-xiao@users.noreply.github.com>
2022-04-16 23:17:11 -0700

    Time: 16 ms (41.34%), Space: 55.7 MB (85.96%) - LeetHub

--- 1762-buildings-with-an-ocean-view/1762-buildings-with-an-ocean-view.java
+++ 1762-buildings-with-an-ocean-view/1762-buildings-with-an-ocean-view.java
@@ -1,27 +1,49 @@
+// class Solution {
+//     public int[] findBuildings(int[] heights) {
+//         int n = heights.length;
+//         boolean[] views = new boolean[n];
+//         Arrays.fill(views, false);
+//         views[n - 1] = true;
+//         int maxHeight = heights[n - 1];
+//         for (int i = n - 2; i >= 0; i--) {
+//             if (heights[i] > heights[i + 1] && heights[i] > maxHeight) {
+//                 views[i] = true;
+//                 maxHeight = heights[i];
+//             }
+//         }
+//         List<Integer> list = new ArrayList<>();
+//         for (int i = 0; i < n; i++) {
+//             if (views[i] == true) {
+//                 list.add(i);
+//             }
+//         }
+//         int[] res = new int[list.size()];
+//         int index = 0;
+//         for (int k : list) {
+//             res[index] = k;
+//             index++;
+//         }
+//         return res;
+//     }
+// }
+
 class Solution {
     public int[] findBuildings(int[] heights) {
         int n = heights.length;
-        boolean[] views = new boolean[n];
-        Arrays.fill(views, false);
-        views[n - 1] = true;
+        Stack<Integer> stack = new Stack<>();
+        stack.push(n - 1);
         int maxHeight = heights[n - 1];
         for (int i = n - 2; i >= 0; i--) {
             if (heights[i] > heights[i + 1] && heights[i] > maxHeight) {
-                views[i] = true;
+                stack.push(i);
                 maxHeight = heights[i];
             }
         }
-        List<Integer> list = new ArrayList<>();
-        for (int i = 0; i < n; i++) {
-            if (views[i] == true) {
-                list.add(i);
-            }
-        }
-        int[] res = new int[list.size()];
-        int index = 0;
-        for (int k : list) {
-            res[index] = k;
-            index++;
+        int[] res = new int[stack.size()];
+        int idx = 0;
+        while (!stack.isEmpty()) {
+            res[idx] = stack.pop();
+            idx++;
         }
         return res;
     }
