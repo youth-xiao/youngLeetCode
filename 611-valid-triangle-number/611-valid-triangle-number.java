042d5cbe285c39072e05fa9d5fe23c2dc480f922
Xiaoyang Zou <54784964+youth-xiao@users.noreply.github.com>
2022-04-24 17:09:41 -0700

    Time: 34 ms (78.04%), Space: 44 MB (35.92%) - LeetHub

--- 611-valid-triangle-number/611-valid-triangle-number.java
+++ 611-valid-triangle-number/611-valid-triangle-number.java
@@ -22,8 +22,10 @@ class Solution { // greedy
     public int triangleNumber(int[] nums) {
         int n = nums.length;
         Arrays.sort(nums);
-        int[] sort = new int[n];
+        int[] sort = new int[n]; // sort as ascending order
+        // can't use Collections.reverseOrder() for primitive data types
         int index = n - 1;
+        // manually reverse it
         for (int i : nums) {
             sort[index] = i;
             index--;
@@ -31,14 +33,18 @@ class Solution { // greedy
         
         int count = 0;
         
+        // int at index a is always the largest one
         for (int a = 0; a < n - 2; a++) {
+            // index b is always the one next to a
             int b = a + 1;
-            int c = n - 1;
+            int c = n - 1; // index c is always start from the end of the sorted array
+            // as long as b and c do not merge
             while (b < c) {
                 if (sort[b] + sort[c] > sort[a]) {
+                    // if sort[b] + sort[c] > sort[a], it means that any numbers larger than sort[c] (any number on the left of sort[c]) can satisfy the condition
                     count += (c - b);
-                    b++;
-                } else {
+                    b++; // shrink the window
+                } else { // this means sort[c] is too small, we're trying to move index c to the left to point to larger int
                     c--;
                 }
             }
