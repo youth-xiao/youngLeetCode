2b1a3325c0eb5d1f14ea56b30e37c85ccddd84aa
Xiaoyang Zou <54784964+youth-xiao@users.noreply.github.com>
2022-04-24 22:15:10 -0700

    Time: 2 ms (32.53%), Space: 40.5 MB (90.72%) - LeetHub

--- 68-text-justification/68-text-justification.java
+++ 68-text-justification/68-text-justification.java
@@ -16,43 +16,18 @@ class Solution {
             sb.deleteCharAt(sb.length() - 1); // remove the last space
             if (sb.length() < maxWidth) {
                 if (wordCount == 1) {
-                        int rightSpaceLen = maxWidth - sb.length();
-                        for (int i = 0; i < rightSpaceLen; i++) {
-                            sb.append(" ");
-                        }
-                        String outcome = sb.toString();
-                        res.add(outcome);
+                    int rightSpaceLen = maxWidth - sb.length();
+                    for (int i = 0; i < rightSpaceLen; i++) {
+                        sb.append(" ");
+                    }
+                    String outcome = sb.toString();
+                    res.add(outcome);
                 } else if ((maxWidth - charNumber) % (wordCount - 1) == 0 && index < words.length) {
                     int spaceLen = (maxWidth - charNumber) / (wordCount - 1);
                     String space = generateSpace(spaceLen);
                     String outcome = sb.toString().replaceAll(" ", space);
                     res.add(outcome);
                 } else if ((maxWidth - charNumber) % (wordCount - 1) != 0 && index < words.length) {
-                    /*
-                    own reason for (3 + 6 + 3 = 12)
-                    maxWidth = 17
-                    (17 - 12) = 5
-                    3 words
-                    2 spaces
-                    
-                    5 / 2 = 2
-                    (5 - 2) / (2 - 1)
-                    
-                    5 / 2 = 2
-                    3 / 1 = 3
-                    
-                    
-                    what you can do (4+3+3+2 = 12)
-                    maxWidth = 16
-                    16 - 12 = 4
-                    4 words, 3 spaces
-                    2, 1, 1
-                    4 / 3 = 1
-                    3 / 2 = 1
-                    2 / 1 = 2
-                    
-                    */
-                    
                     String[] ws = sb.toString().split(" ");
                     int wsLen = ws.length;
                     StringBuilder newSB = new StringBuilder();
@@ -70,17 +45,6 @@ class Solution {
                         wsIndex--;
                     }
                     newSB.insert(0, ws[0]);
-                    
-//                     int smallSpace = (maxWidth - charNumber) / (wordCount - 1);
-//                     int largeSpace = (maxWidth - charNumber - smallSpace) / (wordCount - 2);
-                    
-                    
-//                     String largeSpaceStr = generateSpace(largeSpace);
-//                     String smallSpaceStr = generateSpace(smallSpace);
-//                     for (int i = 0; i < wsLen - 2; i++) {
-//                         newSB.append(ws[i]).append(largeSpaceStr);
-//                     }
-//                     newSB.append(ws[wsLen - 2]).append(smallSpaceStr).append(ws[wsLen - 1]);
                     res.add(newSB.toString());
                 }
                 else if (index == words.length) {
@@ -93,7 +57,6 @@ class Solution {
                 res.add(sb.toString());
             }
         }
-        
         return res;
     }
     
