e3e569fe42ce98141ee01b622a4157f58714278d
Xiaoyang Zou <54784964+youth-xiao@users.noreply.github.com>
2022-05-03 20:03:57 -0700

    Time: 8 ms (12.82%), Space: 42.6 MB (9.70%) - LeetHub

--- 388-longest-absolute-file-path/388-longest-absolute-file-path.java
+++ 388-longest-absolute-file-path/388-longest-absolute-file-path.java
@@ -2,15 +2,17 @@ class Solution {
     public int lengthLongestPath(String input) {
         int maxLen = 0;
         String[] tokens = input.split("\n");
-        int[] depthRecord = new int[input.length() + 1];
+        int[] depthRecord = new int[input.length() + 1]; // not all spots will be necessarily used
+        // just to be safe to create more than enough spaces to store info
         
         for (String s : tokens) {
-            String clean = s.replaceAll("\t", "");
-            int depth = s.length() - clean.length();
-            if (clean.contains(".")) {
+            String clean = s.replaceAll("\t", ""); // \t counts as one character, not two
+            int depth = s.length() - clean.length(); // how many tabs are there
+            if (clean.contains(".")) { // if it is a file, not a directory
                 maxLen = Math.max(maxLen, depthRecord[depth] + clean.length());
-            } else {
-                depthRecord[depth + 1] = depthRecord[depth] + clean.length() + 1;
+            } else { // just a directory
+                // depthRecord[depth]: its parent directory's length
+                depthRecord[depth + 1] = depthRecord[depth] + clean.length() + 1; // +1: need to append "/" because it is not the end of the path if there is/are file(s) within it
             }
         }
         
