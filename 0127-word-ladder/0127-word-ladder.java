c5c69e65c911751cdf5a4a7f7811d871133dbfd8
Xiaoyang Zou <54784964+youth-xiao@users.noreply.github.com>
2022-10-27 15:03:54 -0700

    Time: 156 ms (62.61%), Space: 82.7 MB (49.67%) - LeetHub

--- 0127-word-ladder/0127-word-ladder.java
+++ 0127-word-ladder/0127-word-ladder.java
@@ -1,26 +1,80 @@
 class Solution {
-    public int ladderLength(String beginWord, String endWord, List<String> wordList) {
+//     public int ladderLength(String beginWord, String endWord, List<String> wordList) {
+//         Set<String> wordSet = new HashSet<>(wordList);
+//         if (wordSet.size() == 0 || !wordSet.contains(endWord)) {
+//             return 0;
+//         }
+//         wordSet.remove(beginWord);
+        
+//         Queue<String> queue = new LinkedList<>();
+//         queue.add(beginWord);
+//         Set<String> visited = new HashSet<>();
+//         visited.add(beginWord);
+        
+//         int step = 1;
+//         while (!queue.isEmpty()) {
+//             int currSize = queue.size();
+//             for (int i = 0; i < currSize; i++) {
+//                 String currWord = queue.poll();
+//                 if (changeOneLetter(currWord, endWord, queue, wordSet, visited)) {
+//                     return step + 1;
+//                 }
+//             }
+//             step++;
+//         }
+//         return 0;
+//     }
+    
+    // private boolean changeOneLetter(String currWord, String endWord, 
+    //                                 Queue<String> queue, Set<String> wordSet, Set<String> visited) {
+    //     char[] wordArray = currWord.toCharArray();
+    //     for (int i = 0; i < currWord.length(); i++) {
+    //         char oldChar = wordArray[i];
+    //         for (char k = 'a'; k <= 'z'; k++) {
+    //             if (k == oldChar) { // exclude the same char comparison
+    //                 continue;
+    //             }
+    //             wordArray[i] = k;
+    //             String newWord = String.valueOf(wordArray);
+    //             if (wordSet.contains(newWord)) {
+    //                 if (newWord.equals(endWord)) { // it means it can reach the end
+    //                     return true;
+    //                 }
+    //                 if (!visited.contains(newWord)) {
+    //                     queue.add(newWord);
+    //                     visited.add(newWord);
+    //                 }
+    //             }
+    //         }
+    //         wordArray[i] = oldChar; // restore the original word
+    //     }
+    //     return false;
+    // }
+    
+    public int ladderLength(String beginWord, String endWord, 
+                            List<String> wordList) {
         Set<String> wordSet = new HashSet<>(wordList);
-        if (wordSet.size() == 0 || !wordSet.contains(endWord)) {
+        if (wordSet == null || wordSet.size() == 0 || !wordSet.contains(endWord)) {
             return 0;
         }
         wordSet.remove(beginWord);
         
         Queue<String> queue = new LinkedList<>();
-        queue.add(beginWord);
         Set<String> visited = new HashSet<>();
+        queue.add(beginWord);
         visited.add(beginWord);
         
-        int step = 1;
+        int steps = 1;
+        
         while (!queue.isEmpty()) {
             int currSize = queue.size();
             for (int i = 0; i < currSize; i++) {
                 String currWord = queue.poll();
                 if (changeOneLetter(currWord, endWord, queue, wordSet, visited)) {
-                    return step + 1;
+                    return steps + 1;
                 }
             }
-            step++;
+            steps++;
         }
         return 0;
     }
@@ -28,25 +82,23 @@ class Solution {
     private boolean changeOneLetter(String currWord, String endWord, 
                                     Queue<String> queue, Set<String> wordSet, Set<String> visited) {
         char[] wordArray = currWord.toCharArray();
-        for (int i = 0; i < currWord.length(); i++) {
+        for (int i = 0; i < wordArray.length; i++) {
             char oldChar = wordArray[i];
-            for (char k = 'a'; k <= 'z'; k++) {
-                if (k == oldChar) { // exclude the same char comparison
-                    continue;
-                }
-                wordArray[i] = k;
+            for (char j = 'a'; j <= 'z'; j++) {
+                if (j == oldChar) continue;
+                wordArray[i] = j;
                 String newWord = String.valueOf(wordArray);
                 if (wordSet.contains(newWord)) {
-                    if (newWord.equals(endWord)) { // it means it can reach the end
+                    if (newWord.equals(endWord)) {
                         return true;
                     }
                     if (!visited.contains(newWord)) {
                         queue.add(newWord);
                         visited.add(newWord);
                     }
-                }
+                } 
             }
-            wordArray[i] = oldChar; // restore the original word
+            wordArray[i] = oldChar;
         }
         return false;
     }
