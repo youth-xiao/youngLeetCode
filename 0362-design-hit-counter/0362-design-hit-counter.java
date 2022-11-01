4c86935d4dda84087d18d85f8e9c10460a1d87a2
Xiaoyang Zou <54784964+youth-xiao@users.noreply.github.com>
2022-11-01 13:05:31 -0700

    Time: 2 ms (70.19%), Space: 41.7 MB (75.57%) - LeetHub

--- 0362-design-hit-counter/0362-design-hit-counter.java
+++ 0362-design-hit-counter/0362-design-hit-counter.java
@@ -1,28 +1,53 @@
 class HitCounter {
-
-    Map<Integer, Integer> map;
-    int minTime = -1;
     
+    private Queue<Integer> queue;
+
     public HitCounter() {
-        map = new HashMap<>();
+        this.queue = new LinkedList<>();
     }
     
     public void hit(int timestamp) {
-        map.put(timestamp, map.getOrDefault(timestamp, 0) + 1);
-        if (timestamp < minTime) minTime = timestamp;
+        this.queue.add(timestamp);
     }
     
     public int getHits(int timestamp) {
-        int start = timestamp - 300 + 1;
-        if (start < 1) start = 1;
-        if (start < minTime) start = minTime;
-        int count = 0;
-        for (int i = start; i <= timestamp; i++) {
-            if (map.containsKey(i)) count += map.get(i);
+        while (!this.queue.isEmpty()) {
+            int diff = timestamp - this.queue.peek();
+            if (diff >= 300) {
+                this.queue.remove();
+            } else {
+                break;
+            }
         }
-        return count;
+        return this.queue.size();
     }
 }
+
+// class HitCounter {
+
+//     Map<Integer, Integer> map;
+//     int minTime = -1;
+    
+//     public HitCounter() {
+//         map = new HashMap<>();
+//     }
+    
+//     public void hit(int timestamp) {
+//         map.put(timestamp, map.getOrDefault(timestamp, 0) + 1);
+//         if (timestamp < minTime) minTime = timestamp;
+//     }
+    
+//     public int getHits(int timestamp) {
+//         int start = timestamp - 300 + 1;
+//         if (start < 1) start = 1;
+//         if (start < minTime) start = minTime;
+//         int count = 0;
+//         for (int i = start; i <= timestamp; i++) {
+//             if (map.containsKey(i)) count += map.get(i);
+//         }
+//         return count;
+//     }
+// }
 
 /**
  * Your HitCounter object will be instantiated and called as such:
