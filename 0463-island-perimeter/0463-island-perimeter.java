09c0c26250a965b8a46f4bf8141618f5a16a7080
Xiaoyang Zou <54784964+youth-xiao@users.noreply.github.com>
2022-11-03 14:36:21 -0700

    Time: 18 ms (19.25%), Space: 62.4 MB (40.44%) - LeetHub

--- 0463-island-perimeter/0463-island-perimeter.java
+++ 0463-island-perimeter/0463-island-perimeter.java
@@ -13,20 +13,25 @@ class Solution {
     }
     
     private int dfs(int[][] grid, int i, int j) {
+        // grid 边缘 贡献 1 unit
         if (!withinBound(grid, i, j)) {
             return 1;
         }
         
+        // 遇到海岸线 贡献 1 unit
         if (grid[i][j] == 0) {
             return 1;
         }
         
+        // 遇到已遍历的内陆 0 贡献
         if (grid[i][j] != 1) {
             return 0;
         }
         
+        // 标记已遍历
         grid[i][j] = 2;
         
+        // 这里无需 1+dfs+dfs... 因为不是在算面积 是算边长
         return dfs(grid, i + 1, j) 
                 + dfs(grid, i - 1, j)
                 + dfs(grid, i, j + 1)
