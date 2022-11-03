1317448b0ea116785d5e73133b03f32996830e52
Xiaoyang Zou <54784964+youth-xiao@users.noreply.github.com>
2022-11-03 13:45:09 -0700

    Time: 2 ms (30.04%), Space: 42.5 MB (11.53%) - LeetHub

--- 0678-valid-parenthesis-string/0678-valid-parenthesis-string.java
+++ 0678-valid-parenthesis-string/0678-valid-parenthesis-string.java
@@ -1,30 +1,35 @@
 class Solution {
     public boolean checkValidString(String s) {
+        // NOTE: stacks to store index, NOT char
         Stack<Integer> left = new Stack<>();
         Stack<Integer> star = new Stack<>();
         for (int i = 0; i < s.length(); i++) {
             char ch = s.charAt(i);
+            // see left (, just push
             if (ch == '(') {
                 left.push(i);
-            } else if (ch == '*') {
+            } else if (ch == '*') { // see *, just push, because it can be anything
                 star.push(i);
-            } else {
+            } else { // see right ), need to check a few things
+                // if nothing left in both stacks: nothing to match with )
                 if (left.isEmpty() && star.isEmpty()) {
                     return false;
                 }
+                // pop left stack (higher priority)
                 if (!left.isEmpty()) {
                     left.pop();
-                } else if (!star.isEmpty()) {
+                } else if (!star.isEmpty()) { // we only use * when there is no left ( available to use
                     star.pop();
                 }
             }
         }
-        
+        // need to see if left ( comes before *
         while (!left.isEmpty() && !star.isEmpty()) {
+            // in this case, the order would be: * ( => )(, or ((, or (, none of those have matches
             if (left.pop() > star.pop()) {
                 return false;
             }
         }
-        return left.isEmpty();
+        return left.isEmpty(); // if all parenthesis match, the ( in left stack should be all used up for matching, it is ok to leave star stack non-empty, because * can be ''
     }
 }
